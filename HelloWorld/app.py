22704de1a8d3735640a4b04ac09b581fa2863f82
Ashley Grush <ashleygrush@Ashley-Almeida-Grushs-MacBook-Pro.local>
2026-05-07 19:31:14 -0700

    Initial commit

---
+++ HelloWorld/app.py
@@ -0,0 +1,31 @@
+# First line
+from operator import truediv
+
+print("Hello World")
+
+# Variables - assigns 20 to variable (age). Print output's variable value.
+age = 20
+age = 30
+print(age)
+
+price = 19.95
+first_name = "John"
+last_name = "Smith"
+full_name = f"{first_name} {last_name}"
+is_online = True
+
+# Input function:
+name = input('What is your name? ')
+print("Hello " + name)
+
+
+birth_year = input('What is your birth year? ')
+# Converting string value to int so it can be subtracted
+age = 2026 - int(birth_year)
+print(age)
+
+# Int() - converts to integer
+# Float() - converts to decimal
+# Bool() - converts to true/false
+# str() - converts to string
+
